5e33b4af2922fa5c22c18269f3f11bbfa3059898
klampth <klampth@gmail.com>
2023-11-02 00:22:15 +0200

    first commit

---
+++ new.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4util/libxfce4util-common_4.16.0-1_all.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4util/libxfce4util7_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4util/gir1.2-libxfce4util-1.0_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-bad1.0/gstreamer1.0-plugins-bad-apps_1.20.3-0ubuntu1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-rtp/gstreamer1.0-plugins-rtp_1.14.4.1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/g/gst-rtsp-server1.0/gstreamer1.0-rtsp_1.20.1-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/main/t/taglib/libtagc0_1.11.1%2bdfsg.1-3ubuntu3_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4ui/libxfce4ui-common_4.16.1-1_all.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/x/xfconf/xfconf_4.16.0-2_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/x/xfconf/libxfconf-0-3_4.16.0-2_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4ui/libxfce4ui-2-0_4.16.1-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4util/libxfce4util-bin_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/libx/libxfce4util/libxfce4util-dev_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/p/parole/parole_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/p/parole/parole-dev_4.16.0-1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/v/v4l2-relayd/v4l2-relayd_0.1.2-0ubuntu1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/g/gst-plugins-bad1.0/gstreamer1.0-opencv_1.20.3-0ubuntu1_amd64.deb
---
+++ new01.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/multiverse/n/nvidia-cuda-toolkit/libnvidia-ml-dev_11.5.50%7e11.5.1-1ubuntu1_amd64.deb
+wget -c http://bg.archive.ubuntu.com/ubuntu/pool/universe/u/ubuntu-drivers-common/nvidia-common_0.9.6.2%7e0.22.04.4_amd64.deb
